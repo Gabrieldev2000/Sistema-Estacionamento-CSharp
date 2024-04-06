941613cb33a4926c4dff1ec21715c90bbde1933b
Gabrieldev2000 <gabrielempresaempresa@gmail.com>
2024-04-06 20:07:26 -0300

    first commit

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,83 @@
+namespace Formulas.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(placa))
+            {
+                veiculos.Add(placa);
+                Console.WriteLine($"Veículo com placa {placa} estacionado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Placa inválida. Operação cancelada.");
+            }
+            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
+            // *IMPLEMENTE AQUI*
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine();
+
+            // Verifica se o veículo existe
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                if (int.TryParse(Console.ReadLine(), out int horas) && horas >= 0)
+                {
+                    // Calcular o valor total
+                    decimal precoInicial = 10; // Definir um valor inicial
+                    decimal valorTotal = precoInicial + precoPorHora * horas;
+
+                    // Remover a placa da lista de veículos
+                    veiculos.Remove(placa);
+
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                }
+                else
+                {
+                    Console.WriteLine("Quantidade de horas inválida. Operação cancelada.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            // Verifica se há veículos no estacionamento
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                // Realiza um laço de repetição para exibir os veículos estacionados
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+    SISTEMA DE ESTACIONAMENTO
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+FUNÇÕES ATRIBUIDAS PARA ESTE PROJETO(
+1: Cadastrar veículo |
+2: Remover veículo |
+3: Listar veículos |
+4: Encerrar 
+)
+Curiosidades de algumas funções: na opção 2 remoção de veiculo ele realiza uma comparação
+assim verifica se a placa do carro solicitado existe e ainda exige o valor de horas que o
+veiculo ficou no local para cobrar as taxas.
+
+REQUISITOS:
+
+.NET 6.13 ou acima 
+
+
+DIO PROJECT
